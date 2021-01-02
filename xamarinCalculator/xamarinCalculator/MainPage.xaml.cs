e17f312ceeccb7b30454c827cbf3b38e253dd060
LiamBartolini <liambartolini@gmail.com>
2021-01-02 21:22:08 +0100

    Outupu in lbl

--- xamarinCalculator/xamarinCalculator/MainPage.xaml.cs
+++ xamarinCalculator/xamarinCalculator/MainPage.xaml.cs
@@ -14,5 +14,30 @@ namespace xamarinCalculator
         {
             InitializeComponent();
         }
+
+        private void AggiungiCarattere(object sender, EventArgs e)
+        {
+            lblOutput.Text += (sender as Button).Text;
+        }
+
+        private void CalcolaRisultato(object sender, EventArgs e)
+        {
+            char[] operators = new char[] { '+', '-', '*', '/' };
+
+            // Cerca il simbolo e divide i due numeri e poi fa i calcoli e ritorna il risultato
+            string[] splittedExpression = lblOutput.Text.Split(' ');
+
+            for (int i = 0; i < operators.Length; i++)
+                // Provo a convertire se non riesce ...
+                try
+                {
+                    if (splittedExpression[1].Contains(operators[i]))
+                        lblOutput.Text = splittedExpression[1];
+                }
+                catch
+                {
+                    continue;
+                }
+        }
     }
 }
