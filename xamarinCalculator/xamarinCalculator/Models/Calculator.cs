c0158ac260468bb7c1c3e5a78c47ab63526034ab
LiamBartolini <liambartolini@gmail.com>
2021-01-03 21:32:30 +0100

    todo file

--- xamarinCalculator/xamarinCalculator/Models/Calculator.cs
+++ xamarinCalculator/xamarinCalculator/Models/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -13,12 +14,12 @@ namespace xamarinCalculator.Models
 
         public static string AddingChar(string output, string btnText)
         {
+            // Bracket insert
             if (btnText == "(/)" && !isBracked)
             {
                 isBracked = true;
                 return output += "(";
             }
-            
             if (btnText == "(/)" && isBracked)
             {
                 isBracked = false;
@@ -34,57 +35,70 @@ namespace xamarinCalculator.Models
         public static string CalculateResult(string output)
         {
             char[] operators = new char[] { '+', '-', '*', '/', '%', 's', '!' };
-            string[] strOperators = new string[] { "sum", "subtraction", "multiplication", "division", "mod", "sqrt", "fact"};
+            string[] strOperators = new string[] { "sum", "subtraction", "multiplication", "division", "mod", "sqrt", "fact" };
 
             // find the operator
             List<string> data = new List<string>();
             bool isFind = false;
             string op = "";
+            int cont = 0;
             for (int i = 0; i < operators.Length; i++)
                 if (output.Contains(operators[i].ToString()))
                 {
-                    isFind = true;
                     data = output.Split(operators[i]).ToList();
                     op = strOperators[i];
-                    break;
+                    cont++;
                 }
-                else
-                    isFind = false;
+
+            if (cont >= 1)
+            {
+                Debug.WriteLine("E' UNA ESPRESSIONE");
+                isFind = true;
+            }
+            else if (cont == 0)
+                isFind = false;
 
             if (!isFind)
                 return "Operation doesn't find";
-            else
+
+            double.TryParse(data[0], out double firstOperator);
+            double.TryParse(data[1], out double secondOperator);
+            History = output + " =";
+
+            switch (op)
             {
-                double.TryParse(data[0], out double firstOperator);
-                double.TryParse(data[1], out double secondOperator);
-                History = output + " =";
-                
-                switch(op)
-                {
-                    case "division":
-                        if (data[1] == "0")
-                            return "Division by zero Error!";
-                        else
-                            return (firstOperator / secondOperator).ToString();
-                    case "sum":
+                case "division":
+                    if (data[1] == "0")
+                        return "Division by zero Error!";
+                    else
                         return (firstOperator / secondOperator).ToString();
-                    case "subtraction":
-                        return (firstOperator - secondOperator).ToString();
-                    case "multiplication":
-                        return (firstOperator * secondOperator).ToString();
-                    case "mod":
-                        return (firstOperator % secondOperator).ToString();
-                    case "sqrt":
-                        return Math.Sqrt(firstOperator).ToString();
-                    case "fact":
-                        int f = 1;
-                        for (int i = (int)firstOperator; i < firstOperator - 1; i++)
-                            f *= i;
-                        return f.ToString();
-                    default:
-                        return "Can't resolve";
-                }
+                case "sum":
+                    return (firstOperator + secondOperator).ToString();
+                case "subtraction":
+                    return (firstOperator - secondOperator).ToString();
+                case "multiplication":
+                    return (firstOperator * secondOperator).ToString();
+                case "mod":
+                    return (firstOperator % secondOperator).ToString();
+                case "sqrt":
+                    return Math.Sqrt(firstOperator).ToString();
+                case "fact":
+                    int f = 1;
+                    for (int i = (int)firstOperator; i < firstOperator - 1; i++)
+                        f *= i;
+                    return f.ToString();
+                default:
+                    return "Can't resolve";
             }
+        }
+
+        static void CalculatreExpression(string expression)
+        {
+            char[] higherPriority = new char[] { '*', '/' };
+
+            for (int i = 0; i < higherPriority.Length; i++)
+                if (expression.Contains(higherPriority[i]))
+                    Debug.WriteLine("CAIO");
         }
 
         public static void ClearScreen()
