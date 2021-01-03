786dd36175e7491690b1dc7e5337cff184414704
LiamBartolini <liambartolini@gmail.com>
2021-01-03 20:41:35 +0100

    static class Calculator

--- xamarinCalculator/xamarinCalculator/MainPage.xaml.cs
+++ xamarinCalculator/xamarinCalculator/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using xamarinCalculator.Models;
 
 namespace xamarinCalculator
 {
@@ -16,83 +17,22 @@ namespace xamarinCalculator
             InitializeComponent();
         }
 
-        private void AddingChar(object sender, EventArgs e)
-        {
-            if (lblOutput.Text == "Operation doesn't find" || lblOutput.Text == "Division by zero Error!")
-                lblOutput.Text = (sender as Button).Text;
-            else
-                lblOutput.Text += (sender as Button).Text;
-        }
+        private void AddingChar(object sender, EventArgs e) => lblOutput.Text = Calculator.AddingChar(lblOutput.Text, (sender as Button).Text);
 
         private void CalculateResult(object sender, EventArgs e)
         {
-            char[] operators = new char[] { '+', '-', '*', '/', '%' };
-            string[] strOperators = new string[] { "sum", "subtraction", "multiplication", "division", "mod" };
-
-            // try to find the operator
-            List<string> data = new List<string>();
-            bool isFind = false;
-            string op = "";
-            string expression = lblOutput.Text;
-
-            for (int i = 0; i < operators.Length; i++)
-                if (expression.Contains(operators[i]))
-                {
-                    isFind = true;
-                    data = lblOutput.Text.Split(operators[i]).ToList();
-                    op = strOperators[i];
-                    break;
-                }
-                else
-                    isFind = false;
-
-            if (!isFind)
-                lblOutput.Text = "Operation doesn't find";
-            else
-            {
-                // Faccio i calcoli
-                double.TryParse(data[0], out double firstOperator);
-                double.TryParse(data[1], out double secondOperator);
-                lblHistory.Text = lblOutput.Text + " ="; // Save expression into history label
-                if (op == "division")
-                    if (data[1] == "0")
-                        lblOutput.Text = "Division by zero Error!";
-                    else
-                        lblOutput.Text = Convert.ToString(firstOperator / secondOperator);
-                else if (op == "sum")
-                    lblOutput.Text = Convert.ToString(firstOperator + secondOperator);
-                else if (op == "subtraction")
-                    lblOutput.Text = Convert.ToString(firstOperator - secondOperator);
-                else if (op == "multiplication")
-                    lblOutput.Text = Convert.ToString(firstOperator * secondOperator);
-                else if (op == "mod")
-                    lblOutput.Text = Convert.ToString(firstOperator % secondOperator);
-            }
+            lblOutput.Text = Calculator.CalculateResult(lblOutput.Text);
+            lblHistory.Text = Calculator.History;
         }
 
         private void DeleteExpression(object sender, EventArgs e)
         {
-            lblOutput.Text = null;
-            lblHistory.Text = null;
+            Calculator.ClearScreen();
+            lblOutput.Text = Calculator.Outuput;
+            lblHistory.Text = Calculator.History;
         }
 
-        private void PoppingChar(object sender, EventArgs e)
-        {
-            StringBuilder sb = new StringBuilder();
-            if (lblOutput.Text.Length != 0 || !string.IsNullOrEmpty(lblOutput.Text))
-            {
-                List<char> listExpression = lblOutput.Text.ToList();
-                if (listExpression[listExpression.Count - 1] == ' ')
-                    listExpression.RemoveRange(listExpression.Count - 3, 3);
-                else
-                    listExpression.RemoveAt(listExpression.Count - 1);
-                foreach (char c in listExpression)
-                    sb.Append(c.ToString());
-                lblOutput.Text = sb.ToString();
-            }
-            else
-                lblOutput.Text = null;
-        }
+        private void PoppingChar(object sender, EventArgs e) => lblOutput.Text = Calculator.PoppingChar(lblOutput.Text);
 
         private async void ScientificCalculator(object sender, EventArgs e) => await Navigation.PushModalAsync(new ScientificCalculatorPage());
     }
---
+++ xamarinCalculator/xamarinCalculator/Models/Calculator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xamarinCalculator.Models
+{
+    static class Calculator
+    {
+        public static string History { get; private set; }
+        public static string Outuput { get; private set; }
+
+        public static string AddingChar(string output, string btnText)
+        {
+            if (output == "Operation doesn't find" || output == "Divisio by zero Error")
+                return btnText;
+            else
+                return output += btnText;
+        }
+
+        public static string CalculateResult(string output)
+        {
+            char[] operators = new char[] { '+', '-', '*', '/', '%', 's', '!' };
+            string[] strOperators = new string[] { "sum", "subtraction", "multiplication", "division", "mod", "sqrt", "fact"};
+
+            // find the operator
+            List<string> data = new List<string>();
+            bool isFind = false;
+            string op = "";
+            for (int i = 0; i < operators.Length; i++)
+                if (output.Contains(operators[i].ToString()))
+                {
+                    isFind = true;
+                    data = output.Split(operators[i]).ToList();
+                    op = strOperators[i];
+                    break;
+                }
+                else
+                    isFind = false;
+
+            if (!isFind)
+                return "Operation doesn't find";
+            else
+            {
+                double.TryParse(data[0], out double firstOperator);
+                double.TryParse(data[1], out double secondOperator);
+                History = output + " =";
+                
+                switch(op)
+                {
+                    case "division":
+                        if (data[1] == "0")
+                            return "Division by zero Error!";
+                        else
+                            return (firstOperator / secondOperator).ToString();
+                    case "sum":
+                        return (firstOperator / secondOperator).ToString();
+                    case "subtraction":
+                        return (firstOperator - secondOperator).ToString();
+                    case "multiplication":
+                        return (firstOperator * secondOperator).ToString();
+                    case "mod":
+                        return (firstOperator % secondOperator).ToString();
+                    case "sqrt":
+                        return Math.Sqrt(firstOperator).ToString();
+                    case "fact":
+                        int f = 1;
+                        for (int i = (int)firstOperator; i < firstOperator - 1; i++)
+                            f *= i;
+                        return f.ToString();
+                    default:
+                        return "Can't resolve";
+                }
+            }
+        }
+
+        public static void ClearScreen()
+        {
+            Outuput = null;
+            History = null;
+        }
+
+        public static string PoppingChar(string output)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(output))
+            {
+                List<char> listExpression = output.ToList();
+                if (listExpression[listExpression.Count - 1] == ' ')
+                    listExpression.RemoveRange(listExpression.Count - 3, 3);
+                else
+                    listExpression.RemoveAt(listExpression.Count - 1);
+                foreach (char c in listExpression)
+                    sb.Append(c.ToString());
+                return sb.ToString();
+            }
+
+            return null;
+        }
+    }
+}
